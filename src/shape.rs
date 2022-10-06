6323b5c9781b09eee93d6cd9a20a91cc97fe4ef1
spielhuus <spielhuus@gmail.com>
2022-10-06 12:58:55 +0200

    handle shape size without graphic items

--- src/shape.rs
+++ src/shape.rs
@@ -89,63 +89,61 @@ pub trait Bounds<T> {
 impl Bounds<Array2<f64>> for Symbol {
     fn bounds(&self, libs: &LibrarySymbol) -> Result<Array2<f64>, Error> {
         let mut boundery: Array2<f64> = Array2::default((0, 2));
+        let mut array = Vec::new();
+        let mut rows: usize = 0;
         for symbol in &libs.symbols {
             if self.unit == symbol.unit || symbol.unit == 0 {
-                let mut array = Vec::new();
-                let mut rows: usize = 0;
-                if !symbol.graph.is_empty() {
-                    for element in &symbol.graph {
-                        match element {
-                            Graph::Polyline(polyline) => {
-                                for row in polyline.pts.rows() {
-                                    let x = row[0];
-                                    let y = row[1];
-                                    array.extend_from_slice(&[x, y]);
-                                    rows += 1;
-                                }
+                for element in &symbol.graph {
+                    match element {
+                        Graph::Polyline(polyline) => {
+                            for row in polyline.pts.rows() {
+                                let x = row[0];
+                                let y = row[1];
+                                array.extend_from_slice(&[x, y]);
+                                rows += 1;
                             }
-                            Graph::Rectangle(rectangle) => {
-                                array.extend_from_slice(&[rectangle.start[0], rectangle.start[1]]);
-                                array.extend_from_slice(&[rectangle.end[0], rectangle.end[1]]);
-                                rows += 2;
-                            }
-                            _ => {} //TODO: implement
                         }
-                    }
-                } else {
-                    for pin in &symbol.pin {
-                        array.extend_from_slice(&[pin.at[0], pin.at[1]]);
-                        rows += 1;
+                        Graph::Rectangle(rectangle) => {
+                            array.extend_from_slice(&[rectangle.start[0], rectangle.start[1]]);
+                            array.extend_from_slice(&[rectangle.end[0], rectangle.end[1]]);
+                            rows += 2;
+                        }
+                        _ => {} //TODO: implement
                     }
                 }
-                if rows > 0 {
-                    let array = Array::from_shape_vec((rows, 2), array).unwrap();
-                    let axis1 = array.slice(s![.., 0]);
-                    let axis2 = array.slice(s![.., 1]);
-                    boundery = arr2(&[
-                        [
-                            *axis1
-                                .iter()
-                                .min_by(|a, b| a.partial_cmp(b).unwrap())
-                                .unwrap(),
-                            *axis2
-                                .iter()
-                                .min_by(|a, b| a.partial_cmp(b).unwrap())
-                                .unwrap(),
-                        ],
-                        [
-                            *axis1
-                                .iter()
-                                .max_by(|a, b| a.partial_cmp(b).unwrap())
-                                .unwrap(),
-                            *axis2
-                                .iter()
-                                .max_by(|a, b| a.partial_cmp(b).unwrap())
-                                .unwrap(),
-                        ],
-                    ]);
+                for pin in &symbol.pin {
+                    array.extend_from_slice(&[pin.at[0], pin.at[1]]);
+                    rows += 1;
                 }
             }
+        }
+        if rows > 0 {
+            let array = Array::from_shape_vec((rows, 2), array).unwrap();
+            let axis1 = array.slice(s![.., 0]);
+            let axis2 = array.slice(s![.., 1]);
+            boundery = arr2(&[
+                [
+                    *axis1
+                        .iter()
+                        .min_by(|a, b| a.partial_cmp(b).unwrap())
+                        .unwrap(),
+                    *axis2
+                        .iter()
+                        .min_by(|a, b| a.partial_cmp(b).unwrap())
+                        //.min_by(|a, b| a.partial_cmp(b).unwrap())
+                        .unwrap(),
+                ],
+                [
+                    *axis1
+                        .iter()
+                        .max_by(|a, b| a.partial_cmp(b).unwrap())
+                        .unwrap(),
+                    *axis2
+                        .iter()
+                        .max_by(|a, b| a.partial_cmp(b).unwrap())
+                        .unwrap(),
+                ],
+            ]);
         }
         Ok(boundery)
     }
@@ -164,7 +162,7 @@ mod tests {
         let symbol = doc.get_symbol("U1", 1).unwrap();
         let lib_symbol = doc.get_library("Amplifier_Operational:TL072").unwrap();
         let size = symbol.bounds(lib_symbol).unwrap();
-        assert_eq!(arr2(&[[-5.08, -5.08], [5.08, 5.08]]), size)
+        assert_eq!(arr2(&[[-7.62, -5.08], [7.62, 5.08]]), size)
     }
     #[test]
     fn shape_opamp_c() {
@@ -173,5 +171,13 @@ mod tests {
         let lib_symbol = doc.get_library("Amplifier_Operational:TL072").unwrap();
         let size = symbol.bounds(lib_symbol).unwrap();
         assert_eq!(arr2(&[[-2.54, -7.62], [-2.54, 7.62]]), size)
+    }
+    #[test]
+    fn shape_r() {
+        let doc = Schema::load("files/opamp.kicad_sch").unwrap();
+        let symbol = doc.get_symbol("R1", 1).unwrap();
+        let lib_symbol = doc.get_library("Device:R").unwrap();
+        let size = symbol.bounds(lib_symbol).unwrap();
+        assert_eq!(arr2(&[[-1.016, -3.81], [1.016, 3.81]]), size)
     }
 }
